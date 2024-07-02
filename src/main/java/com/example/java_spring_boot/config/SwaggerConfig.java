72971511108cd35b12274622a1b8bb39a9fa40ec
Amanda Chou <wmzup124@gmail.com>
2024-07-02 22:23:25 +0800

    feat: add token and @Hidden

--- src/main/java/com/example/java_spring_boot/config/SwaggerConfig.java
+++ src/main/java/com/example/java_spring_boot/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.example.java_spring_boot.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +18,22 @@ public class SwaggerConfig {
                 .description("Spring Boot REST API for Java")
                 .version("1.0.0")
                 .description("List of Java Spring Boot REST APIs");
-        return new OpenAPI().info(info);
+
+        String securitySchemaName = "JWT Authentication";
+        SecurityRequirement securityRequirement =
+                new SecurityRequirement().addList(securitySchemaName);
+        Components components = new Components()
+                .addSecuritySchemes(securitySchemaName,
+                        new SecurityScheme()
+                                .name(securitySchemaName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                );
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
